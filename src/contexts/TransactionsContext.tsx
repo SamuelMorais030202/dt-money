223bcbb8ed233e4e347b725e1de27583e557b397
Samuel Morais030202 <moraissamuel009@gmail.com>
2025-01-24 09:18:07 -0300

    feat: criando funcionalidade de pesquisa

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -10,7 +10,8 @@ export interface ITransactions {
 }
 
 interface ITransactionsContextType {
-  transactions: ITransactions[]
+  transactions: ITransactions[];
+  fetchTransactions: (query?: string) => Promise<void>;
 }
 
 interface ITransactionsProvider {
@@ -23,19 +24,25 @@ export const TransactionsContext = createContext({} as ITransactionsContextType)
 export function TransactionsProvider({ children }: ITransactionsProvider) {
   const [transactions, setTransactions] = useState<ITransactions[]>([]);
   
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3333/transactions');
+  async function fetchTransactions(query?: string) {
+    const url = new URL('http://localhost:3333/transactions');
+
+    if (query) {
+      url.searchParams.append('q', query);
+    }
+
+    const response = await fetch(url);
     const data = await response.json();
 
     setTransactions(data);
   }
   
   useEffect(() => {
-    loadTransactions();
+    fetchTransactions();
   }, [])
 
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
       {children}
     </TransactionsContext.Provider>
   )
